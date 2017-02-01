624be89f678a4e6a1f696d649c1db096e05d71c5
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-02-02 02:09:48 +0900

    refactor

--- main.go
+++ main.go
@@ -8,38 +8,43 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"time"
 
+	"github.com/mattn/go-colorable"
+	"github.com/mattn/go-tty"
 	"github.com/mitchellh/go-homedir"
 
 	"golang.org/x/crypto/ssh"
 )
 
 var (
-	user       = flag.String("u", "", "user")
-	password   = flag.String("p", "", "password")
-	privateKey = flag.String("f", "", "private key")
-	port       = flag.Int("P", 22, "port")
+	user        = flag.String("u", "", "user")
+	password    = flag.String("p", "", "password")
+	askPassword = flag.Bool("w", false, "ask password")
+	privateKey  = flag.String("f", "", "private key")
+	port        = flag.Int("P", 22, "port")
 )
 
-func getSigner(keyPath string) (key ssh.Signer, err error) {
-	home, err := homedir.Dir()
+func pprompt(prompt string) (string, error) {
+	t, err := tty.Open()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
+	fmt.Print(prompt)
+	return t.ReadPassword()
+}
 
-	if keyPath == "" {
-		keyPath = filepath.Join(home, ".ssh", "id_rsa")
-	}
-	buf, err := ioutil.ReadFile(keyPath)
+func getSigners(keyfile string, password string) ([]ssh.Signer, error) {
+	buf, err := ioutil.ReadFile(keyfile)
 	if err != nil {
 		return nil, err
 	}
 
 	b, _ := pem.Decode(buf)
 	if x509.IsEncryptedPEMBlock(b) {
-		buf, err = x509.DecryptPEMBlock(b, []byte(*password))
+		buf, err = x509.DecryptPEMBlock(b, []byte(password))
 		if err != nil {
 			return nil, err
 		}
@@ -51,13 +56,13 @@ func getSigner(keyPath string) (key ssh.Signer, err error) {
 		if err != nil {
 			return nil, err
 		}
-		return k, nil
+		return []ssh.Signer{k}, nil
 	}
 	k, err := ssh.ParsePrivateKey(buf)
 	if err != nil {
 		return nil, err
 	}
-	return k, nil
+	return []ssh.Signer{k}, nil
 }
 
 func run() int {
@@ -66,17 +71,41 @@ func run() int {
 		flag.Usage()
 		return 2
 	}
-
-	var authMethods []ssh.AuthMethod
-
-	authMethods = append(authMethods, ssh.Password(*password))
-	if *privateKey != "" {
-		signer, err := getSigner(*privateKey)
+	if *privateKey == "" {
+		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			return 1
 		}
-		authMethods = append(authMethods, ssh.PublicKeys(signer))
+		*privateKey = filepath.Join(home, ".ssh", "id_rsa")
+	}
+	if *user == "" {
+		if runtime.GOOS == "windows" {
+			*user = os.Getenv("USERNAME")
+		} else {
+			*user = os.Getenv("USER")
+		}
+	}
+
+	var authMethods []ssh.AuthMethod
+
+	authMethods = append(authMethods, ssh.PasswordCallback(func() (string, error) {
+		if *askPassword {
+			return pprompt("password: ")
+		}
+		return *password, nil
+	}))
+	if *privateKey != "" {
+		authMethods = append(authMethods, ssh.PublicKeysCallback(func() ([]ssh.Signer, error) {
+			if *askPassword {
+				p, err := pprompt("password: ")
+				if err != nil {
+					return nil, err
+				}
+				*password = p
+			}
+			return getSigners(*privateKey, *password)
+		}))
 	}
 
 	config := &ssh.ClientConfig{
@@ -88,14 +117,14 @@ func run() int {
 	hostport := fmt.Sprintf("%s:%d", flag.Arg(0), *port)
 	conn, err := ssh.Dial("tcp", hostport, config)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "cannot connect %v: %v", hostport, err)
+		fmt.Fprintf(os.Stderr, "cannot connect %v: %v\n", hostport, err)
 		return 1
 	}
 	defer conn.Close()
 
 	session, err := conn.NewSession()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "cannot open new session: %v", err)
+		fmt.Fprintf(os.Stderr, "cannot open new session: %v\n", err)
 		return 1
 	}
 	defer session.Close()
@@ -105,8 +134,8 @@ func run() int {
 		conn.Close()
 	}()
 
-	session.Stdout = os.Stdout
-	session.Stderr = os.Stderr
+	session.Stdout = colorable.NewColorableStdout()
+	session.Stderr = colorable.NewColorableStderr()
 	session.Stdin = os.Stdin
 	err = session.Run(strings.Join(flag.Args()[1:], " "))
 	if err != nil {
